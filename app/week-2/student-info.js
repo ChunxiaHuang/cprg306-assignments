a6ad00039facbac17eb07cb6f15bb77e279ce162
ChunxiaHuang <springsummeryellow@gmail.com>
2024-05-14 17:25:38 -0600

    Create student info component
    Created StudentInfo component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <h1>Chunxia Huang</h1>
+            <Link href="https://github.com/ChunxiaHuang/cprg306-assignments">https://github.com/ChunxiaHuang/cprg306-assignments</Link>
+        </div>
+    );
+}
