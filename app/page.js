d33ce72eba2be89c23408908411a9dcbd6c196a3
Chunxia Huang <chunxia.huang@edu.sait.ca>
2024-06-05 14:49:18 -0600

    Create page week 4
    add link of week 4 to main page
    create component new-item
    create page for week 4

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <ul>
         <li className="text-lg pl-20 text-teal-100"><Link href="./week-2">Week-2 Assignment</Link></li>
         <li className="text-lg pl-20 text-teal-100"><Link href="./week-3">Week-3 Assignment</Link></li>
+        <li className="text-lg pl-20 text-teal-100"><Link href="./week-4">Week-4 Assignment</Link></li>
       </ul>
     </main>
   );
---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+
+export default function Page(){
+    return(
+        <main className="bg-teal-950 flex justify-center h-screen">
+            <NewItem/>
+        </main>
+    );
+}
