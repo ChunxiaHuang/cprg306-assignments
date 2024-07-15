9674393586451a2c03960b6a6459bf022b5c250d
Chunxia Huang <chunxia.huang@edu.sait.ca>
2024-07-14 22:03:25 -0600

    update list item with hover colour

--- app/week-7/item.js
+++ app/week-7/item.js
@@ -5,7 +5,7 @@ export default function Item({item, onSelect}){
     let {name, quantity, category} = item;
 
     return (
-        <div className="text-teal-900 bg-teal-100 m-4 p-2 w-96"
+        <div className="text-teal-900 bg-teal-100 m-4 p-2 w-96 hover:bg-yellow-500"
             onClick={()=>onSelect(name)} >
             <ul>
                 <li className="text-xl font-bold">{name}</li>
